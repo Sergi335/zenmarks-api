d41af2a10eb31e4ad2f585a8b40cf4ea09a2e7fb
Sergi335 <sergiadn335@gmail.com>
2025-07-10 09:09:31 +0200

    feat: agregar cross-env y mejorar la estructura del servidor

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import 'dotenv/config'
+import { dbConnect } from './config/mongodb'
 
 const app = express()
 export default app
 app.use(express.json())
 const port = process.env.PORT ?? 3000
 
-app.get('/', (_, res) => {
+app.get('/', (_: Request, res: Response) => {
   res.send('Hello World!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+void (async () => {
+  await dbConnect()
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+})()
